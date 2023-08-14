ae5750185fced305ca52e03e6732eb13d4733b10
Deveshwar Jaiswal <deveshwar.jaiswal996@gmail.com>
2023-08-14 11:25:54 +0530

    added quiz ui

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,15 +1,15 @@
-// import HistoryCard from "@/components/dashboard/HistoryCard";
-// import QuizMeCard from "@/components/dashboard/QuizMeCard";
+import HistoryCard from "@/components/dashboard/HistoryCard";
+import QuizMeCard from "@/components/dashboard/QuizMeCard";
 import { getAuthSession } from "@/lib/nextauth";
 import { redirect } from "next/navigation";
 import React from "react";
-// import HotTopicsCard from "./HotTopicsCard";
-// import RecentActivities from "./RecentActivities";
+import HotTopicsCard from "./HotTopicsCard";
+import RecentActivities from "./RecentActivities";
 
 type Props = {};
 
 export const metadata = {
-    title: "Dashboard | Quizmify",
+    title: "Dashboard | Quiz App",
 };
 
 const Dashboard = async (props: Props) => {
@@ -23,15 +23,15 @@ const Dashboard = async (props: Props) => {
                 <h2 className="mr-2 text-3xl font-bold tracking-tight">Dashboard</h2>
             </div>
 
-            {/*<div className="grid gap-4 mt-4 md:grid-cols-2">*/}
-            {/*    <QuizMeCard />*/}
-            {/*    <HistoryCard />*/}
-            {/*</div>*/}
+            <div className="grid gap-4 mt-4 md:grid-cols-2">
+                <QuizMeCard />
+                <HistoryCard />
+            </div>
 
-            {/*<div className="grid gap-4 mt-4 md:grid-cols-2 lg:grid-cols-7">*/}
-            {/*    <HotTopicsCard />*/}
-            {/*    <RecentActivities />*/}
-            {/*</div>*/}
+            <div className="grid gap-4 mt-4 md:grid-cols-2 lg:grid-cols-7">
+                <HotTopicsCard />
+                <RecentActivities />
+            </div>
         </main>
     );
 };
--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -4,15 +4,17 @@ import { SessionProvider } from "next-auth/react";
 import React from "react";
 import {ThemeProvider, ThemeProvider as NextThemesProvider} from "next-themes";
 import { type ThemeProviderProps } from "next-themes/dist/types";
-// import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-//
-// const queryClient = new QueryClient();
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 const Providers = ({ children, ...props }: ThemeProviderProps) => {
     return (
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-            <SessionProvider>{children}</SessionProvider>
-        </ThemeProvider>
+        <QueryClientProvider client={queryClient}>
+            <NextThemesProvider attribute="class" defaultTheme="system" enableSystem{...props}>
+                <SessionProvider>{children}</SessionProvider>
+            </NextThemesProvider>
+        </QueryClientProvider>
 
     );
 };
