a57a426f5ed3daebb463d6030c646e67e3401576
md-amir-gauhar <gauharamir15@gmail.com>
2023-08-21 23:34:21 +0530

    user avatar dropdown dark theme ui fix

--- src/components/UserAccountNav.tsx
+++ src/components/UserAccountNav.tsx
@@ -1,66 +1,66 @@
 "use client";
 
 import {
-    DropdownMenu,
-    DropdownMenuContent,
-    DropdownMenuItem,
-    DropdownMenuSeparator,
-    DropdownMenuTrigger,
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import {LogOut} from "lucide-react";
-import type {User} from "next-auth";
-import {signOut} from "next-auth/react";
+import { LogOut } from "lucide-react";
+import type { User } from "next-auth";
+import { signOut } from "next-auth/react";
 import Link from "next/link";
 import React from "react";
 import UserAvatar from "./UserAvatar";
 
 type Props = {
-    user: Pick<User, "name" | "image" | "email">;
+  user: Pick<User, "name" | "image" | "email">;
 };
 
-const UserAccountNav = ({user}: Props) => {
-    return (
-        <DropdownMenu>
-            <DropdownMenuTrigger>
-                <UserAvatar
-                    className="w-10 h-10"
-                    user={{
-                        name: user.name || null,
-                        image: user.image || null,
-                    }}
-                />
-            </DropdownMenuTrigger>
-            <DropdownMenuContent className="bg-white" align="end">
-                <div className="flex items-center justify-start gap-2 p-2">
-                    <div className="flex flex-col space-y-1 leading-none">
-                        {user.name && <p className="font-medium">{user.name}</p>}
-                        {user.email && (
-                            <p className="w-[200px] truncate text-sm text-zinc-700">
-                                {user.email}
-                            </p>
-                        )}
-                    </div>
-                </div>
-                <DropdownMenuSeparator/>
-                <DropdownMenuItem asChild>
-                    <Link href="/">Deva Quiz</Link>
-                </DropdownMenuItem>
+const UserAccountNav = ({ user }: Props) => {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <UserAvatar
+          className="w-10 h-10"
+          user={{
+            name: user.name || null,
+            image: user.image || null,
+          }}
+        />
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="bg-white dark:bg-[#030712]" align="end">
+        <div className="flex items-center justify-start gap-2 p-2">
+          <div className="flex flex-col space-y-1 leading-none">
+            {user.name && <p className="font-medium">{user.name}</p>}
+            {user.email && (
+              <p className="w-[200px] truncate text-sm text-zinc-700 dark:text-white">
+                {user.email}
+              </p>
+            )}
+          </div>
+        </div>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem asChild>
+          <Link href="/">Deva Quiz</Link>
+        </DropdownMenuItem>
 
-                <DropdownMenuSeparator/>
+        <DropdownMenuSeparator />
 
-                <DropdownMenuItem
-                    onSelect={(event) => {
-                        event.preventDefault();
-                        signOut().catch(console.error);
-                    }}
-                    className="text-red-600 cursor-pointer"
-                >
-                    Sign out
-                    <LogOut className="w-4 h-4 ml-2 "/>
-                </DropdownMenuItem>
-            </DropdownMenuContent>
-        </DropdownMenu>
-    );
+        <DropdownMenuItem
+          onSelect={(event) => {
+            event.preventDefault();
+            signOut().catch(console.error);
+          }}
+          className="text-red-600 cursor-pointer hover:text-red-600"
+        >
+          Sign out
+          <LogOut className="w-4 h-4 ml-2 " />
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
 };
 
 export default UserAccountNav;
